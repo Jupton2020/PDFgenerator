310469217a61c4cd20a7219e8ae20a2fc9bb3419
jack upt <jhonbahama@gmail.com>
2020-01-02 20:15:50 -0800

    created two dependencies

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 const questions = ["What is your Github user name?","Choose your favorite color"];
 
 function writeToFile(fileName, data) {
